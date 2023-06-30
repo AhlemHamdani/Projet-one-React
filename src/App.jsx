f32c9e5460ce1b9f79de506c60f46752a7281123
Ahlem Hamdani <ahlem.hamdani.ca@gmail.com>
2023-06-29 20:35:16 -0400

    finaliser les scripts et les css

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './App.css';
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <PokemonCard name="Pikachu" abilities={['Static', 'Lightning Rod']} />
+      <PokemonCard name="Weedle" abilities={['Shield Dust', 'Run Away']} />
+      <PokemonCard name="Pidgey" abilities={['Keen Eye or Tangled', 'Big Pecks']} />
+      <PokemonCard name="Rattata" abilities={['Run Away or Guts', 'Thick Fat']} />
+      <PokemonCard name="Oddish" abilities={['Run Away', 'Chlorophyll']} />
+      <PokemonCard name="Bulbasaur" abilities={['Overgrow', 'Chlorophyll']} />
+      <PokemonCard name="Charizard" abilities={['Blaze', 'Solar Power']} />
+      <PokemonCard name="Squirtle" abilities={['Torrent', 'Rain Dish']} />
+      <PokemonCard name="Jigglypuff" abilities={['Cute Charm', 'Competitive']} />
+      <PokemonCard name="Caterpie" abilities={['Shield Dust', 'Run Away']} />
+      
+    </div>
+  );
+};
+    
+  
+
+export default App
